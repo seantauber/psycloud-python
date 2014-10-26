060e511faac797082c24a5e0006c63c3a4f86c96
Sean Tauber <seanetauber@gmail.com>
2014-10-26 15:11:08 +1030

    seting up as a deployable package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='psycloud',
+      version='0.1',
+      description='psycloud python client and utilities',
+      url='http://github.com/seantauber/psycloud-python',
+      author='Sean Tauber',
+      author_email='seanetauber@gmail.com',
+      license='MIT',
+      packages=['psycloud'],
+      zip_safe=False)
