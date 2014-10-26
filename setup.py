4e86ea8edad0b7212a168794ac5ae24dcf2fb059
Sean Tauber <seanetauber@gmail.com>
2014-10-26 16:12:55 +1030

    modified setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='psycloud',
+setup(name='psycloud-python',
       version='0.1',
       description='psycloud python client and utilities',
       url='http://github.com/seantauber/psycloud-python',
