dd11dd6bd86ad09a83364ff7a4aeeac34d863bb4
Sean Tauber <seanetauber@gmail.com>
2014-10-26 16:20:41 +1030

    working on deployment

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='psycloud-python',
+setup(name='psycloud_python',
       version='0.1',
       description='psycloud python client and utilities',
       url='http://github.com/seantauber/psycloud-python',
       author='Sean Tauber',
       author_email='seanetauber@gmail.com',
       license='MIT',
-      packages=['psycloud-python'])
+      packages=['psycloud_python'])
